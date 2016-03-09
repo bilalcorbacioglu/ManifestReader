63aa65e165be3bbf8e7bb18dc7b48b0c7f442d91
bilalcorbacioglu <bilalcorbacioglu@gmail.com>
2016-03-09 23:27:12 +0200

    ADD XAP and update Readme(XAP)

---
+++ Manifest.Reader.XAP/Program.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using Ionic.Zip;
+using System.IO;
+using System.Xml;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+using System.Collections.Generic;
+
+namespace Manifest.Reader.XAP
+{
+    [Serializable(), XmlRoot("Deployment")]
+    public class Deployment
+    {
+        [XmlElement("App")]
+        public App App { get; set; }
+
+        [XmlAttribute("AppPlatformVersion")]
+        public String AppPlatformVersion { get; set; }
+    }
+
+    public class App
+    {
+        [XmlAttribute("Version")]
+        public String Version { get; set; }
+
+        [XmlAttribute("Description")]
+        public String Description { get; set; }
+
+        //[XmlElement("IconPath")]
+        //public IconPath IconPath { get; set; }
+
+        [XmlArray("Capabilities")]
+        public Capability[] Capabilities { get; set; }
+
+        //[XmlElement("Tasks")]
+        //public Tasks Tasks { get; set; }
+    }
+
+    public class Capability
+    {
+        [XmlAttribute("Name")]
+        public String Name { get; set; }
+    }
+
+    /*
+    public class IconPath
+    {
+        [XmlAttribute("IsRelative")]
+        public Boolean IsRelative { get; set; }
+
+        [XmlAttribute("IsResource")]
+        public Boolean IsResource { get; set; }
+    }
+
+    public class Tasks
+    {
+        [XmlElement("DefaultTask")]
+        public DefaultTask DefaultTask { get; set; }
+
+    }
+
+    public class DefaultTask
+    {
+        [XmlAttribute("Name")]
+        public String Name { get; set; }
+
+        [XmlAttribute("NavigationPage")]
+        public String NavigationPage { get; set; }
+
+    }*/
+
+    public class NamespaceIgnorantXmlTextReader : XmlTextReader
+    {
+        public NamespaceIgnorantXmlTextReader(Stream reader) : base(reader) { }
+
+        public override String NamespaceURI
+        {
+            get { return String.Empty; }
+        }
+    }
+
+    public class ProgramXAP
+    {
+        public string path;
+        public ProgramXAP() {}
+        public void xap(string path)
+        {
+            ZipEntry entry = new ZipEntry();
+            using (ZipFile zip = ZipFile.Read(path))
+            {
+                entry = zip["WMAppManifest.xml"];
+            }
+
+            using (MemoryStream stream = new MemoryStream())
+            {
+                entry.Extract(stream);
+                stream.Position = 0;
+                var serializer = new XmlSerializer(typeof(Deployment));
+                var deployment = serializer.Deserialize(new NamespaceIgnorantXmlTextReader(stream)) as Deployment;
+                System.Console.WriteLine(deployment.App.Version);
+                System.Console.WriteLine(deployment.App.Description);
+                System.Console.WriteLine(deployment.App.Capabilities);
+                System.Console.Read();
+            }
+
+        }
+
+        static void Main ( string[] args )
+        {
+            ZipEntry entry = new ZipEntry();
+            System.Console.WriteLine("Enter Path");
+
+            string path = System.Console.ReadLine();
+            
+            using (ZipFile zip = ZipFile.Read(path))                        
+            {
+                entry = zip["WMAppManifest.xml"];
+            }
+
+            using (MemoryStream stream = new MemoryStream())
+            {
+                entry.Extract(stream);
+                stream.Position = 0;
+                var serializer = new XmlSerializer(typeof(Deployment));
+                var deployment = serializer.Deserialize(new NamespaceIgnorantXmlTextReader(stream)) as Deployment;
+                System.Console.WriteLine(deployment.App.Version);
+                System.Console.WriteLine(deployment.App.Description);
+                System.Console.WriteLine(deployment.App.Capabilities);
+                System.Console.Read();
+            }
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,27 @@
-# ManifestReader
-Read the manifest of the application on all platforms.
+# Manifest Reader
+**Read the manifest of the application on all platforms.**
+
+Manifest Reader ona verdiğiniz uygulamanın ( APK, XAP, IPA ) manifest verilerini okumanızı sağlar. Bu sayede uygulamalarınızın Beta test & Production ve Enterprise süreçlerinde manifest bilgilerini okumada size kolaylık sağlar.
+
+## Usage and Examples (Kullanım ve Örnek)
+### XAP Manifest Reader
+XAP uygulamaların manifest bilgileri içinde bir çok tag yer almakta. Proje içinde kullanıcağınız tagleri kod kısmında düzenleyebilirsiniz. Ufak bir örnek verelim.
+
+[![image](http://i.hizliresim.com/v54dlD.png)](http://hizliresim.com/v54dlD)
+
+(31,32)(37,38) Satırlarda görüldüğü gibi tercihe göre yeni bir attribute eklenebilir.
+Ayrıca ihtiyaca göre yeni bir class'ta tanımlayabilirsiniz.
+```sh
+public class IconPath
+    {
+        [XmlAttribute("IsRelative")]
+        public Boolean IsRelative { get; set; }
+
+        [XmlAttribute("IsResource")]
+        public Boolean IsResource { get; set; }
+    }
+```
+gibi kullanılabilinir. 
+
+* Burada class'ların ve attribute'lerin ne manaya geldiğini anlamadıysanız WMAppManifest.xml dosyasını incelemenizi tavsiye ederim. 
+* Ayrıca XmlElement ve XmlAttribute gibi kavramlara yabancı iseniz **System. Xml.Serialization** kütüphanesini incelemenizi öneririm.
