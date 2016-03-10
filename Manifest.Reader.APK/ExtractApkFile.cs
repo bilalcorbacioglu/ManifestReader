ad2897e44dbaa9419f8658563b2c6f9f7f8a5e10
bilalcorbacioglu <bilalcorbacioglu@gmail.com>
2016-03-10 14:30:46 +0200

    Manifest(APK) Read

---
+++ Manifest.Reader.APK/ExtractApkFile.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ICSharpCode;
+using Iteedee.ApkReader;
+using System.IO;
+
+namespace Manifest.Reader.APK
+{
+    public class ExtractApkFile
+    {
+        public static void ExtractFileAndSave(string APKFilePath)
+        {
+            using (ICSharpCode.SharpZipLib.Zip.ZipInputStream zip = new ICSharpCode.SharpZipLib.Zip.ZipInputStream(File.OpenRead(APKFilePath)))
+            {
+                using (var filestream = new FileStream(APKFilePath, FileMode.Open, FileAccess.Read))
+                {
+                    ICSharpCode.SharpZipLib.Zip.ZipFile zipfile = new ICSharpCode.SharpZipLib.Zip.ZipFile(filestream);
+                }
+            }
+        }
+    }
+}
---
+++ Manifest.Reader.APK/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ICSharpCode;
+using Iteedee.ApkReader;
+using System.IO;
+
+namespace Manifest.Reader.APK
+{
+    class ProgramAPK
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter Path");
+            string APKfilePath = Console.ReadLine();
+            ApkInfo info = ReadApk.ReadApkFromPath(APKfilePath);
+            Console.ReadKey();
+        }
+    }
+}
